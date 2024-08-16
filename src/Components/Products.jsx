d35830c4490cbd04e249ceaec37d9160151bdf12
rashed62 <mdrashedbiswas83@gmail.com>
2024-08-16 23:28:54 +0600

    create pagination button

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,11 +1,23 @@
 import { useEffect, useState } from "react";
 import SingleProduct from "./SingleProduct";
 import { useLoaderData } from "react-router-dom";
+import './products.css'
 
 const Products = () => {
     const [product, setProduct] = useState([]);
     const {count} = useLoaderData();
-    console.log(count)
+    const itemsPerPage = 10;
+    const numberOfPages = Math.ceil(count / itemsPerPage);
+
+    const pages = []
+    for (let i =0; i < numberOfPages; i++) {
+        pages.push(i)
+    }
+    console.log(pages)
+    // shortcut
+    // const pages = [...Array(numberOfPages).keys()];
+
+
 
     useEffect(() => {
         fetch('http://localhost:5000/products')
@@ -14,10 +26,17 @@ const Products = () => {
     }, [])
 
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 lg:mt-10">
+        <div>
+            <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 lg:mt-10">
            {
             product.map(aProduct => <SingleProduct key={aProduct._id} aProduct={aProduct}></SingleProduct>)
            }
+        </div>
+        <div>
+           {
+            pages.map(page => <button className="btn btn-primary" key={page}>{page}</button>)
+           }
+        </div>
         </div>
     );
 };

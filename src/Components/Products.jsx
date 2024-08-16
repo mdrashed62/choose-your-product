a40dbb5e88bd10e66a2defa5fe804d65eb81ce6d
rashed62 <mdrashedbiswas83@gmail.com>
2024-08-17 04:47:49 +0600

    almost

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,44 +1,89 @@
 import { useEffect, useState } from "react";
 import SingleProduct from "./SingleProduct";
 import { useLoaderData } from "react-router-dom";
-import './products.css'
+import "./products.css";
 
 const Products = () => {
-    const [product, setProduct] = useState([]);
-    const {count} = useLoaderData();
-    const itemsPerPage = 10;
-    const numberOfPages = Math.ceil(count / itemsPerPage);
+  const [product, setProduct] = useState([]);
+  const [itemPerPage, setItemPerPage] = useState(8);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [asc, setAsc] = useState(true);
+  const data = useLoaderData();
+  const {count} = data || {}
+  const numberOfPages = Math.ceil(count / itemPerPage);
 
-    const pages = []
-    for (let i =0; i < numberOfPages; i++) {
-        pages.push(i)
+  const pages = [];
+  for (let i = 0; i < numberOfPages; i++) {
+    pages.push(i + 1);
+  }
+  console.log(pages);
+  // shortcut
+  // const pages = [...Array(numberOfPages).keys()];
+
+  useEffect(() => {
+    fetch(` http://localhost:5000/products?page=${currentPage}&size=${itemPerPage}&sort=${asc? 'asc' : 'desc'}`)
+      .then((res) => res.json())
+      .then((data) => setProduct(data));
+  }, [currentPage, itemPerPage, asc]);
+
+  const handleItemPerPage = (e) => {
+    const val = parseInt(e.target.value);
+    console.log(val);
+    setItemPerPage(val);
+    setCurrentPage(1);
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
     }
-    console.log(pages)
-    // shortcut
-    // const pages = [...Array(numberOfPages).keys()];
+  };
 
+  const handleNextPage = () => {
+    if (currentPage < pages.length - 1) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
 
-
-    useEffect(() => {
-        fetch('http://localhost:5000/products')
-        .then(res =>res.json())
-        .then(data =>setProduct(data));
-    }, [])
-
-    return (
-        <div>
-            <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 lg:mt-10">
-           {
-            product.map(aProduct => <SingleProduct key={aProduct._id} aProduct={aProduct}></SingleProduct>)
-           }
-        </div>
-        <div>
-           {
-            pages.map(page => <button className="btn btn-primary" key={page}>{page}</button>)
-           }
-        </div>
-        </div>
-    );
+  return (
+    <div>
+       <button onClick={() => setAsc(!asc)}
+       className="btn btn-secondary">
+        {asc ? 'Price: Low to High' : 'Price: High to Low'}
+        </button>
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 lg:mt-10">
+        {product.map((aProduct) => (
+          <SingleProduct key={aProduct._id} aProduct={aProduct}></SingleProduct>
+        ))}
+      </div>
+      <div className="flex items-center gap-4 pagination mt-6 justify-center mb-2">
+        <button onClick={handlePrevPage}>Previous</button>
+        {pages.map((page) => (
+          <button
+            onClick={() => setCurrentPage(page)}
+            className={
+              currentPage === page
+                ? "selected text-white px-3 py-1 rounded-md"
+                : undefined
+            }
+            key={page}
+          >
+            {page}
+          </button>
+        ))}
+        <button onClick={handleNextPage}>Next</button>
+        <select
+          value={itemPerPage}
+          onChange={handleItemPerPage}
+          className="bg-slate-300"
+        >
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="20">20</option>
+        </select>
+      </div>
+    </div>
+  );
 };
 
 export default Products;
--- src/Components/SingleProduct.jsx
+++ src/Components/SingleProduct.jsx
@@ -12,31 +12,38 @@ const SingleProduct = ({ aProduct }) => {
     creationDate,
   } = aProduct;
 
+ 
+  const numericPrice = parseFloat(price);
+
+  const formattedDate = new Date(creationDate).toISOString().split('T')[0];
+
   return (
-    <div className="card card-compact bg-base-100 shadow-xl">
+    <div className="card card-compact bg-base-100">
       <figure>
         <img className="w-full h-[400px]"
           src={image}
-          alt="Shoes"
+          alt={name}
         />
       </figure>
       <div className="card-body">
         <div className="flex items-center w-full justify-between">
-        <h2 className="card-title">{name}</h2>
-        <div className="text-end">
-        <p className="gap-2 flex items-center">{ratings} <FaRegStar className="text-yellow-600"/></p>
-        </div>
+          <h2 className="card-title">{name}</h2>
+          <div className="text-end">
+            <p className="gap-2 flex items-center">
+              {ratings} <FaRegStar className="text-yellow-600" />
+            </p>
+          </div>
         </div>
         <p>{description}</p>
         <div>
-            <div className="flex">
-                <p>{category}</p>
-                <p className="text-end">{brand}</p>
-            </div>
-            <div className="flex">
-                <p>{creationDate}</p>
-                <p className="text-end lg:text-xl font-semibold">{price}</p>
-            </div>
+          <div className="flex justify-between">
+            <p>{category}</p>
+            <p>{brand}</p>
+          </div>
+          <div className="flex justify-between">
+            <p>{formattedDate}</p>
+            <p className=" font-bold">${numericPrice.toFixed(2)}</p>
+          </div>
         </div>
       </div>
     </div>
